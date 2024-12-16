a78a87569e7f15ca2b36ffefa9b815a9343f8030
EmanuelCN <emanuelghub@gmail.com>
2024-12-16 17:46:05 +0000

    sm8550-common: Decommonize thermal config

--- common.mk
+++ common.mk
@@ -404,9 +404,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.sony
